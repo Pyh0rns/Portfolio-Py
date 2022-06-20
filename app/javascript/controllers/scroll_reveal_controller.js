98ee800527ca150d85ee31822d9078d14f3dabb1
PierreYves Hoorens <pyhoorens@gmail.com>
2022-06-20 11:26:45 +0200

    scroll reveal WIP

--- app/javascript/controllers/scroll_reveal_controller.js
+++ app/javascript/controllers/scroll_reveal_controller.js
@@ -1,5 +1,33 @@
-import { Application } from '@hotwired/stimulus'
+// import { Application } from '@hotwired/stimulus'
+// import ScrollReveal from 'stimulus-scroll-reveal'
+
+// const application = Application.start()
+// application.register('scroll-reveal', ScrollReveal)
+
+
 import ScrollReveal from 'stimulus-scroll-reveal'
 
-const application = Application.start()
-application.register('scroll-reveal', ScrollReveal)
+export default class extends ScrollReveal {
+  connect() {
+    super.connect()
+  }
+
+  // Override this method to change the IntersectionObserver behavior
+  intersectionObserverCallback(entries, observer) {
+    //
+  }
+
+  // Options used for the IntersectionObserver
+  get intersectionObserverOptions() {
+    return {}
+  }
+
+  // You can override this getter to set your default options here.
+  get defaultOptions() {
+    return {
+      class: 'active',
+      threshold: 0.5,
+      rootMargin: '100px',
+    }
+  }
+}
