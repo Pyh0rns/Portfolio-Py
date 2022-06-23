ff444d8016a349476ae1a21d8eecee6c0febe72b
PierreYves Hoorens <pyhoorens@gmail.com>
2022-06-23 12:46:10 +0200

    added typed js

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+// Don't forget to import the NPM package
+import Typed from "typed.js"
+
+export default class extends Controller {
+  connect() {
+    new Typed(this.element, {
+      strings: ["Premiere phrase", "Deuxieme..."],
+      typeSpeed: 120,
+      loop: true
+    });
+  }
+}
